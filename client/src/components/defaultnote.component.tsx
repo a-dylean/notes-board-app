e681dd3be4508c0e835e1713779381da4745f2fb
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-09-24 15:14:45 +0200

    Added ui folder

--- client/src/components/defaultnote.component.tsx
+++ client/src/components/defaultnote.component.tsx
@@ -3,19 +3,22 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { BASE_URL } from "../../appconfig";
 import { api } from "@/app/api/actions";
+import { SaveIcon } from "@/app/ui/icons/save.icon";
+import { TrashIcon } from "@/app/ui/icons/trash.icon";
 
 export const DefaultNote = () => {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("hello");
   const [content, setContent] = useState("");
   const queryClient = useQueryClient();
-  const {mutate: addNote, isLoading} = useMutation({
-    mutationFn: async () => await api.post(`${BASE_URL}/notes`, {title, content}),
+  const { mutate: addNote, isLoading } = useMutation({
+    mutationFn: async () =>
+      await api.post(`${BASE_URL}/notes`, { title, content }),
     onSuccess: () => {
-      setTitle("")
-      setContent("")
-      queryClient.invalidateQueries(['notes'])
-    }
-  })
+      setTitle("");
+      setContent("");
+      queryClient.invalidateQueries(["notes"]);
+    },
+  });
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
@@ -25,75 +28,39 @@ export const DefaultNote = () => {
   const clearInput = () => {
     setTitle("");
     setContent("");
-  }
+  };
   return (
-
-      <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-        <div>
-          {/* <form className="w-full max-w-sm" > */}
-            <div className="flex items-center border-b  w-full border-gray-400 mb-6 py-1 px-1">
-              <input
-                className="appearance-none bg-transparent border-none w-full text-gray-700 leading-tight focus:outline-none"
-                type="text"
-                placeholder="New note"
-                aria-label="Note title"
-                value={title}
-                onChange={handleInputChange}
-                disabled={isLoading}
-              />
-            </div>
-          {/* </form> */}
-          {/* <form> */}
-            <textarea
-              className="h-full min-h-[100px] w-full resize-none border-transparent"
-              placeholder="Write your thoughts here..."
-              value={content}
-              onChange={handleTextAreaChange}
+    <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
+      <div>
+        <form className="w-full max-w-sm">
+          <div className="flex items-center border-b  w-full border-gray-400 mb-6 py-1 px-1">
+            <input
+              className="appearance-none bg-transparent border-none w-full text-gray-700 leading-tight focus:outline-none"
+              type="text"
+              placeholder="New note"
+              aria-label="Note title"
+              value={title}
+              onChange={handleInputChange}
               disabled={isLoading}
-            ></textarea>
-          {/* </form> */}
-          {/* <p className="text-gray-800 dark:text-gray-100 text-sm "></p> */}
-        </div>
-          <div className="flex items-center text-gray-800 dark:text-gray-100">
-            <button type="button" onClick={()=>addNote()}>
-              <div className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-check"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M5 12l5 5l10 -10"></path>
-                </svg>
-              </div>
-            </button>
-            <button type="button" onClick={clearInput}>
-            <div className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-x"
-                width={20}
-                height={20}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M18 6l-12 12"></path>
-                <path d="M6 6l12 12"></path>
-              </svg>
-            </div></button>
+            />
           </div>
+          <textarea
+            className="h-full min-h-[100px] w-full resize-none border-transparent"
+            placeholder="Write your thoughts here..."
+            value={content}
+            onChange={handleTextAreaChange}
+            disabled={isLoading}
+          ></textarea>
+        </form>
       </div>
+      <div className="flex items-center text-gray-800 dark:text-gray-100">
+        <button type="button" onClick={() => addNote()}>
+          <SaveIcon />
+        </button>
+        <button type="button" onClick={clearInput}>
+          <TrashIcon />
+        </button>
+      </div>
+    </div>
   );
 };
--- client/src/components/usernote.component.tsx
+++ client/src/components/usernote.component.tsx
@@ -4,8 +4,15 @@ import { getRandomColor } from "../app/getRandomColor";
 import { Note } from "@prisma/client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+import { TrashIcon } from "../app/ui/icons/trash.icon";
+import { EditIcon } from "@/app/ui/icons/edit.icon";
+import { SaveIcon } from "@/app/ui/icons/save.icon";
 
 export const UserNote = ({ id, title, content, created, edited }: Note) => {
+  const [editMode, setEditMode] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+  const [newContent, setNewContent] = useState(content);
   const time = new Date(edited ? edited : created).toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
@@ -14,7 +21,8 @@ export const UserNote = ({ id, title, content, created, edited }: Note) => {
   const queryClient = useQueryClient();
 
   const { mutate: updateNote, isLoading: updateLoading } = useMutation({
-    mutationFn: async () => await api.put(`notes/${id}`, { title, content }),
+    mutationFn: async () =>
+      await api.put(`notes/${id}`, { title: newTitle, content: newContent }),
     onSuccess: () => {
       queryClient.invalidateQueries(["notes"]);
     },
@@ -27,65 +35,76 @@ export const UserNote = ({ id, title, content, created, edited }: Note) => {
       queryClient.invalidateQueries(["notes"]);
     },
   });
-  return (
-      <div
-        className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4"
-        style={{
-          backgroundColor: getRandomColor().backgroundColor
-        }}
-      >
-        <div>
-          <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3">
-            {title}
-          </h4>
+  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNewContent(e.target.value);
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(e.target.value);
+  };
 
-          <p className="text-gray-800 dark:text-gray-100 text-sm ">{content}</p>
-        </div>
-        <div>
-          <div className="flex items-center justify-between text-gray-800 dark:text-gray-100">
-            <p className="text-sm">Last edited: {time}</p>
-            <button onClick={() => updateNote()}>
-              <div className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-pencil"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
-                  <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
-                </svg>
-              </div>
-            </button>
-            <button type="button" onClick={() => deleteNote()}>
-              <div className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-x"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M18 6l-12 12"></path>
-                  <path d="M6 6l12 12"></path>
-                </svg>
-              </div>
-            </button>
+  return (
+    <div
+      className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4"
+      // style={{
+      //   backgroundColor: getRandomColor().backgroundColor
+      // }}
+    >
+      <div>
+        {editMode ? (
+          <form className="w-full max-w-sm">
+            <div className="flex items-center border-b  w-full border-gray-400 mb-6 py-1 px-1">
+              <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 leading-tight focus:outline-none"
+                type="text"
+                placeholder={title}
+                value={newTitle}
+                aria-label="Note title"
+                onChange={handleInputChange}
+                disabled={isLoading}
+              />
+            </div>
+            <textarea
+              className="h-full min-h-[100px] w-full resize-none border-transparent"
+              placeholder="Write your thoughts here..."
+              value={newContent}
+              onChange={handleTextAreaChange}
+              disabled={isLoading}
+            ></textarea>
+          </form>
+        ) : (
+          <div>
+            <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3">
+              {title}
+            </h4>
+            <p className="text-gray-800 dark:text-gray-100 text-sm ">
+              {content}
+            </p>
           </div>
+        )}
+      </div>
+      <div>
+        <div className="flex items-center justify-between text-gray-800 dark:text-gray-100">
+          <p className="text-sm">Last edited: {time}</p>
+          {editMode ? (
+            <button
+              type="button"
+              onClick={() => {
+                updateNote();
+                setEditMode(false);
+              }}
+            >
+              <SaveIcon />
+            </button>
+          ) : (
+            <button onClick={() => setEditMode(true)}>
+              <EditIcon />
+            </button>
+          )}
+          <button type="button" onClick={() => deleteNote()}>
+            <TrashIcon />
+          </button>
         </div>
       </div>
+    </div>
   );
 };
