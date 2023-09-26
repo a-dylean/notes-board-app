541082ec30ba551130880d7c3c6bd5aa9f624c23
Alice Tonkopiy <adtonkopiy@gmail.com>
2023-09-26 11:10:27 +0200

    Disabled dark mode

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,11 +2,13 @@ import { join } from 'path'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [join(__dirname, 'src/**/*.{js,jsx,ts,tsx,html,css}')],
-  important: true,
+  content: [
+    join(__dirname, 'src/**/*.{js,jsx,ts,tsx,html,css}')
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: ['class'],
 }
 export default config
